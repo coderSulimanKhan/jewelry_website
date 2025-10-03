eef7f0db98d4f98ac81abea009cefbd381013b76
Suliman Khan <suliman.soft.eng@gmail.com>
2025-10-03 18:40:26 +0500

    build user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["customer", "admin"], default: "customer" },
+  address: { country: String, city: String, street: String, postalCode: String },
+  phone: String,
+  wishlist: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+  image: { type: String, default: "avatar.png" }
+});
+
+const User = model("User", userSchema);
+
+export default User;
